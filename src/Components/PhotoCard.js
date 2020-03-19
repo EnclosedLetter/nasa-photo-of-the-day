aed9485e92e8c0eaab597e0476491c4bb7b69243
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-19 14:46:39 -0700

    cant display it

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+
+const PhotoCard = () => {
+  const [apod, setApod] = useState();
+
+  useEffect(() => {
+
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=HxKpxviDJLToIBzuk69LUkM6R3UhitfEZgXL7Xhd")
+    .then(response => {
+      // console.log(`PhotoCard: response: `response)
+        setApod(response.data.url);
+    })
+    
+  }, [])
 
 
-const PhotoCard = props => {
     return (
       <div className="">
         <img className="" alt="" src={} />
         <h2>{props.breed}</h2>
       </div>
     );
+
+
+    return <img src={apod} alt = " pic of athe day" />;
   };
   
   export default PhotoCard;
