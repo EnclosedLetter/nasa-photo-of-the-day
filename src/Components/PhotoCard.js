7ec1612856a5152e33be57884da5950a7e10ee8b
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-19 14:17:23 -0700

    .

---
+++ src/Components/PhotoCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const PhotoCard = props => {
+    return (
+      <div className="">
+        <img className="" alt="" src={} />
+        <h2>{props.breed}</h2>
+      </div>
+    );
+  };
+  
+  export default PhotoCard;
+  
