df718a0019197102c1608f347f9095caf7e6263b
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-19 17:00:59 -0700

    working on implimenting the other components

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,22 +1,34 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Date from "./Date";
 
 const PhotoCard = () => {
-  const [apod, setApod] = useState();
+  const [apod, setApod] = useState({});
 
   useEffect(() => {
 
     axios
     .get("https://api.nasa.gov/planetary/apod?api_key=HxKpxviDJLToIBzuk69LUkM6R3UhitfEZgXL7Xhd")
     .then(response => {
-      console.log(response)
-        setApod(response.data.url);
+      console.log(response.data)
+        setApod(response.data);
     })
     .catch(error => console.log("Photo not found", error));
   }, []);
+  console.log(apod)
+    return <img src={apod.url} alt = "pic of the day" />;
 
-    return <img src={apod} alt = "pic of the day" />;
+    // <Date 
+    //   title={apod.title}
+    //   date={apod.date}
+    //   explain={apod.explanation}
+    //   img={apod.url}
+    //   copyright={apod.copyright}
+    // />
+
   };
   
   export default PhotoCard;
+
+
   
